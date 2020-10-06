8f866c233f53f489130aa0eca7c9226d2e45a8c7
byf <baiyunfei@firstbrave.com>
2020-10-06 17:11:45 +0800

    init project

---
+++ src/main/java/com/application/Application.java
@@ -0,0 +1,20 @@
+package com.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+/**
+ * @author 一元咖啡
+ * @site www.zhulu.com
+ * @create 2020-10-06 17:04
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/application/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 一元咖啡
+ * @site www.zhulu.coms
+ * @create 2020-10-06 17:07
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/t1")
+    public String t1(String name) {
+        return "hi:" + name;
+    }
+}
